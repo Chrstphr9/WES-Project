2cc710e6b11dc1a3463c79d9999b6c187ec2002b
somto <somto565@gmail.com>
2025-02-25 08:36:46 +0100

    `Update App component layout and content`

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,18 @@ function App() {
 
   return (
     <>
-     <div className='w-full max-w-[1440px] mx-auto bg-white'>
-        <h2 className='text-xl font-semibold mb-2 text-center'>Your Education</h2>
-        <p className='text-lg text-gray-500 mb-4 text-center'>Tell us about your academic background</p>
-        
+     <div className='relative pt-10 h-[870px] w-full max-w-[600px] mx-auto bg-white'>
+        <div className='text-center'>
+        <h2 className='text-[24px] font-bold mb-2 mt-4'>Your Education</h2>
+        <p className='text-[16px] text-gray-500 mb-4 mt-4 font-normal'>Tell us about your academic background</p>
+        <p className='text-blue-500 font-semibold text-[16px]'>800 WESPoint remaining to unlock ESA</p>
+          <div className='mx-auto max-w-[552px] h-[73px] bg-[#E6EEF7] rounded-lg mt-4 flex items-center justify-center'>
+            <p className='text-[#0256B2] text-[18px] font-extrabold'>Your Reward for this step is 200 WESPoints</p>
+          </div>       
+        </div>
+
+
+        {/* Form */}
      </div>
     </>
   )
