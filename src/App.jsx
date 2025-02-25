6cff2cd95f5879ac37eaac2db464043ab763c23f
somto <somto565@gmail.com>
2025-02-25 07:27:53 +0100

    `feat: updated App.jsx and index.css files with new content and Tailwind CSS configurations`

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
+  const [progress, setProgress] = useState(25);
+  const [formData, setFormData] = useState({})
  
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <div className='w-full max-w-[1440px] mx-auto bg-white'>
+        <h2 className='text-xl font-semibold mb-2 text-center'>Your Education</h2>
+        <p className='text-lg text-gray-500 mb-4 text-center'>Tell us about your academic background</p>
+        
+     </div>
     </>
   )
 }
